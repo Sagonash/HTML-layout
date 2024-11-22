60e00d360bce0b912ade5f99ef0cb1b5b0fe69d8
Sagonash <ryazanskiy.maxim@yandex.ru>
2024-11-22 22:01:20 +0300

    Added moving slider with arrows

--- script.js
+++ script.js
@@ -36,4 +36,30 @@ window.onload=function(){
             dropdown.style.transform = "rotate(360deg)";
         }
     });
+
+    let back = document.getElementById('Back');
+    let forward = document.getElementById('Forward');
+
+    back.addEventListener('mousedown', function(){
+        let newLeft = div.offsetLeft - (border.offsetWidth - div.offsetWidth) / 4;
+        console.log(newLeft);
+        if (newLeft < 0){
+            div.style.left = 0;
+        }
+        else{
+            div.style.left = newLeft + 'px';
+        }
+    });
+
+    forward.addEventListener('mousedown', function(){
+        let rightEdge = border.offsetWidth;
+        let newRight = div.offsetLeft + div.offsetWidth + (border.offsetWidth - div.offsetWidth) / 4;
+        console.log(newRight);
+        if (newRight > rightEdge){
+            div.style.left = rightEdge - div.offsetWidth + 'px';
+        }
+        else{
+            div.style.left = newRight - div.offsetWidth + 'px';
+        }
+    });
 }
